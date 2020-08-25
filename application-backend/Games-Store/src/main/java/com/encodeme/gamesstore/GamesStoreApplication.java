dc8166a96c8b240446bc76c975976807e94ad842
sas-arkadiusz <kontakt.sas.arkadiusz@gmail.com>
2020-08-25 21:12:05 +0200

    Initialized application backend with Spring Boot

---
+++ application-backend/Games-Store/src/main/java/com/encodeme/gamesstore/GamesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.encodeme.gamesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesStoreApplication.class, args);
+	}
+
+}
---
+++ application-backend/Games-Store/src/test/java/com/encodeme/gamesstore/GamesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encodeme.gamesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
