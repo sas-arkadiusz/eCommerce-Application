3809776f4d24fde135e61e0d2fe6f31c17dfd3a2
sas-arkadiusz <kontakt.sas.arkadiusz@gmail.com>
2020-08-29 14:18:03 +0200

    Added new entity classes for Game and GameGenre tables

---
+++ application-backend/Games-Store/src/main/java/com/encodeme/gamesstore/entity/Game.java
@@ -0,0 +1,53 @@
+package com.encodeme.gamesstore.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "game")
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", nullable = false)
+    private GameGenre genre;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @CreationTimestamp
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+}
---
+++ application-backend/Games-Store/src/main/java/com/encodeme/gamesstore/entity/GameGenre.java
@@ -0,0 +1,25 @@
+package com.encodeme.gamesstore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "game_genre")
+@Getter
+@Setter
+public class GameGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "genre_name")
+    private String genreName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "genre")
+    private Set<Game> games;
+}
