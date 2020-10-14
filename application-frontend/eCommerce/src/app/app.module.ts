e67777b6295c3170600214fb307ac59aeafcebc0
sas-arkadiusz <kontakt.sas.arkadiusz@gmail.com>
2020-10-14 19:00:21 +0200

    Added game-list Component to the Frontend

--- application-frontend/eCommerce/src/app/app.module.ts
+++ application-frontend/eCommerce/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule
